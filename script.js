12f739dbd569cb4f24f464244745f164fafe1a3f
utsav <sav.acharya22@gmail.com>
2024-02-23 16:46:06 +1100

    show score of player1

--- script.js
+++ script.js
@@ -3,10 +3,42 @@
 // Selecting Elements
 const score0El = document.querySelector('#score--0')
 const score1El = document.querySelector('#score--1')
+const current0El = document.querySelector('#current--0')
+const current1El = document.querySelector('#current--1')
+
 const diceEl = document.querySelector('.dice')
+const btnNew = document.querySelector('.btn--new')
+const btnRoll = document.querySelector('.btn--roll')
+const btnHold = document.querySelector('.btn--hold')
 
 score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden')
 
 
+ let currentScore = 0
+
+// Rolling dice functionality
+
+btnRoll.addEventListener('click', function () {
+
+    // GENERATE A RANDOM DICE
+    const dice = Math.floor(Math.random() * 6) + 1;
+
+//    Display hidden
+
+    diceEl.classList.remove('hidden');
+    diceEl.src = `dice-${dice}.png`;
+
+            //  Ckeack for roll 1 : if true swich nect player
+
+    if (dice !== 1) {
+        // add dice to current score
+        currentScore += dice;
+        current0El.textContent = currentScore
+    }else {
+        // switch to next player
+
+    }
+    
+})
